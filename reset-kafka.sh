09f4e6b33575a29236c517f1c1ed6091da42946a
Ray Liu <aliu150@my.bcit.ca>
2025-04-02 20:04:18 -0700

    change to docker-compose

--- reset-kafka.sh
+++ reset-kafka.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Stop the services
-docker compose stop kafka zookeeper
+docker-compose stop kafka zookeeper
 
 # Remove the meta.properties file
 rm -f ./data/kafka/meta.properties
 
 # Start the services again
-docker compose start zookeeper
+docker-compose start zookeeper
 sleep 5
 docker compose start kafka
