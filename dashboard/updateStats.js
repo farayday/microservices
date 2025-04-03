cead107ab328cff1b6e54a2772336387b27c1f49
Ray Liu <aliu150@my.bcit.ca>
2025-04-03 14:40:11 -0700

    update dashboard

---
+++ dashboard/updateStats.js
@@ -0,0 +1,70 @@
+/* UPDATE THESE VALUES TO MATCH YOUR SETUP */
+
+const PROCESSING_STATS_API_URL =
+  "http://microservices-app.westus2.cloudapp.azure.com:8100/stats";
+const ANALYZER_API_URL = {
+  stats:
+    "http://microservices-app.westus2.cloudapp.azure.com:8110/traffic/stats",
+  conditions:
+    "http://microservices-app.westus2.cloudapp.azure.com:8110/traffic/conditions?index=latest",
+  incident:
+    "http://microservices-app.westus2.cloudapp.azure.com:8110/traffic/incidents?index=latest",
+};
+
+// This function fetches and updates the general statistics
+const makeReq = (url, cb) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+      console.log("Received data: ", result);
+      cb(result);
+    })
+    .catch((error) => {
+      updateErrorMessages(error.message);
+    });
+};
+
+const updateCodeDiv = (result, elemId) =>
+  (document.getElementById(elemId).innerText = JSON.stringify(result));
+
+const getLocaleDateStr = () => new Date().toLocaleString();
+
+const getStats = () => {
+  document.getElementById("last-updated-value").innerText = getLocaleDateStr();
+
+  makeReq(PROCESSING_STATS_API_URL, (result) =>
+    updateCodeDiv(result, "processing-stats")
+  );
+  makeReq(ANALYZER_API_URL.stats, (result) =>
+    updateCodeDiv(result, "analyzer-stats")
+  );
+  makeReq(ANALYZER_API_URL.conditions, (result) =>
+    updateCodeDiv(result, "event-conditions")
+  );
+  makeReq(ANALYZER_API_URL.incident, (result) =>
+    updateCodeDiv(result, "event-incident")
+  );
+};
+
+const updateErrorMessages = (message) => {
+  const id = Date.now();
+  console.log("Creation", id);
+  msg = document.createElement("div");
+  msg.id = `error-${id}`;
+  msg.innerHTML = `<p>Something happened at ${getLocaleDateStr()}!</p><code>${message}</code>`;
+  document.getElementById("messages").style.display = "block";
+  document.getElementById("messages").prepend(msg);
+  setTimeout(() => {
+    const elem = document.getElementById(`error-${id}`);
+    if (elem) {
+      elem.remove();
+    }
+  }, 7000);
+};
+
+const setup = () => {
+  getStats();
+  setInterval(() => getStats(), 4000); // Update every 4 seconds
+};
+
+document.addEventListener("DOMContentLoaded", setup);
